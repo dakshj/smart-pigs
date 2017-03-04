3cd1a5a6999392be8a70eee14193c8261745addb
Daksh Jotwani <dakshjay@gmail.com>
2017-03-04 00:23:33 -0500

    Add JavaDoc to Main.java's psvm

--- smartpigs/src/main/java/com/smartpigs/Main.java
+++ smartpigs/src/main/java/com/smartpigs/Main.java
@@ -8,9 +8,29 @@ import java.rmi.RemoteException;
 
 public class Main {
 
+    /**
+     * @param args <p>
+     *             args[0]:
+     *             <p>
+     *             Use "0" to start a Game Server.
+     *             <p>
+     *             Use "1" to start a Pig Server.
+     *             <p>
+     *             <p>
+     *             args[1]:
+     *             <p>
+     *             The port number to start the server on.
+     *             <p>
+     *             <p>
+     *             args[2]: <i>(Only required when starting a Game Server)</i>
+     *             <p>
+     *             The file path of the configuration file.
+     * @throws RemoteException Thrown when a Java RMI exception occurs
+     */
     public static void main(String[] args) throws RemoteException {
         if (args == null || args.length == 0) {
-            throw new IllegalArgumentException("No arguments provided.");
+            throw new IllegalArgumentException("No command-line arguments provided." +
+                    " Please refer the JavaDoc to know more on these arguments.");
         }
 
         ExecutionMode executionMode;
